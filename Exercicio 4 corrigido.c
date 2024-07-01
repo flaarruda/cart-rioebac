cf29cc020b6ca2bdeee58efcfdd68940c4561a2b
flaarruda <flavia.arruda92@gmail.com>
2024-07-01 20:31:22 -0300

    Opção de Saída
    Incluso no sistema a opção de saída para o usuário

--- Exercicio 4 corrigido.c
+++ Exercicio 4 corrigido.c
@@ -113,6 +113,7 @@ int main()
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consultar nomes\n");
 	printf("\t3 - Deletar nomes\n\n");
+	printf("\t4 - Sair do Sistema\n\n");
 	printf("Opção: "); // fim do menu
 	
 	scanf("%d", &opcao); // armazenando a escolha do usuário
@@ -136,6 +137,11 @@ int main()
 		 deletar();
 		 system("pause");
 		 break;
+		 
+		 case 4:
+		 printf("Obrigada por utilizar o Sistema!\n");
+		 return 0;
+		 break;	
 		 
 		 default:
 		 printf("Essa opção não está disponível!\n");
